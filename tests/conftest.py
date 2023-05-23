7d898d98a3c83d9352ab2bea578d7af2ffb7559f
German <germanmartinezayuso@gmail.com>
2023-05-23 15:12:25 +0000

    Adding timeout

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from testbook import testbook
 
 @pytest.fixture(scope="module")
 def tb():
-    with testbook("./notebook.ipynb", execute=True) as tb:
+    with testbook("./notebook.ipynb", execute=True, timeout=9999) as tb:
         yield tb
